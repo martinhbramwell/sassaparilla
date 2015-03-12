619591181d342c7e3b9d8a37b96d6df5a73f97ac
Martin H. Bramwell <martinhbramwell@gmail.com>
2015-03-12 13:11:46 -0400

    Work on packager script for deploying to server

--- example.config.sh
+++ example.config.sh
@@ -4,9 +4,12 @@
 export MONGO_URL=mongodb://localhost:27017/sassaparilla
 
 ## Build and deploy settings
-export BUILD_DIRECTORY=.meteor/shipping/
 export TARGET_SERVER=www.fleetingclouds.com
-export ZIPALIGN_PATH=~/.meteor/android_bundle/android-sdk/build-tools/21.0.0/
+export ZIPALIGN_PATH=~/.meteor/android_bundle/android-sdk/build-tools/21.0.0
 export ALIGNMENT=4
+
+export BUILD_DIRECTORY=../tmp
+
+
 
 
--- packager.sh
+++ packager.sh
@@ -5,44 +5,60 @@ if [ ! -f "config.sh" ]; then
   echo "cp example.config.sh config.sh "
   exit
 fi
-
+#
 export PROJ=${PWD##*/}
-
+#
 source config.sh
 export TARGET_SERVER_URL="https://${TARGET_SERVER}/"
-
+#
+export TARGET_DIRECTORY=${BUILD_DIRECTORY}/${PROJ}
+mkdir -p ${TARGET_DIRECTORY}
+#
 echo "### Configuration for your '"${PROJ}"' project is :"
 echo "   ~                                      Target Server is  : " ${TARGET_SERVER_URL}
 echo "   ~ Align android-sdk bundle on "$ALIGNMENT"-byte boundary when using : " $ZIPALIGN_PATH
 echo "   ~                              Temporary build directory : " ${BUILD_DIRECTORY}
 echo "### ~   ~   ~    " 
-
-mkdir -p ${BUILD_DIRECTORY}
-
+#
+echo "Checking/installing Android capabilities : "
+meteor install-sdk android
+#
+echo ""
+echo ""
+echo "**NOT** Checking/installing iOS capabilities : "
+#meteor install-sdk ios
+#
+echo ""
+echo ""
 echo "Building project : ${PROJ} in ${BUILD_DIRECTORY}"
-exit
-meteor build ${BUILD_DIRECTORY}        --server=https://www.fleetingclouds.com/
-mv ${BUILD_DIRECTORY}/android/unaligned.apk ${BUILD_DIRECTORY}/android/${PROJ}_unaligned.apk
-meteor build ${BUILD_DIRECTORY} --debug --server=https://www.fleetingclouds.com/
+meteor build ${TARGET_DIRECTORY}         --server=${TARGET_SERVER_URL}
+mv ${TARGET_DIRECTORY}/android/unaligned.apk ${TARGET_DIRECTORY}/android/${PROJ}_unaligned.apk
+meteor build ${TARGET_DIRECTORY} --debug --server=${TARGET_SERVER_URL}
 #
 #
-pushd ${BUILD_DIRECTORY}
+pushd ${TARGET_DIRECTORY}
 pushd ./android > /dev/null
+echo "Sign the unaligned APK"
 jarsigner -storepass 34erDFCV-*-* -tsa http://timestamp.digicert.com -digestalg SHA1 ${PROJ}_unaligned.apk ${PROJ}
-${ZIPALIGN_PATH}/zipalign -f -v ${ALIGNMENT} ${PROJ}_unaligned.apk ${PROJ}_aligned.apk
-# ${ZIPALIGN_PATH}/zipalign -c -v ${ALIGNMENT} ${PROJ}_aligned.apk
+echo "Align to byte boundaries and verify."
+${ZIPALIGN_PATH}/zipalign -f    ${ALIGNMENT} ${PROJ}_unaligned.apk ${PROJ}_aligned.apk
+# ${ZIPALIGN_PATH}/zipalign -f -v ${ALIGNMENT} ${PROJ}_unaligned.apk ${PROJ}_aligned.apk
+echo ".........  "
 #
+echo "Rename and relocate for easy deployment"
 mv ${PROJ}_aligned.apk ..
 mv unaligned.apk ../${PROJ}.apk
 popd > /dev/null
 #
-echo "Uploading . . . "
-scp ${PROJ}.* www.fleetingclouds.com:~/incoming
+echo "Uploading ${PROJ}.tar.gz & ${PROJ}.apk to . . . "
+pwd
+scp ${PROJ}.* ${TARGET_SERVER}:~/incoming
 #
 echo "Installing . . . "
-ssh www.fleetingclouds.com "sudo -u meteor /home/meteor/installProj.sh ${PROJ}"
+ssh ${TARGET_SERVER} "sudo -u meteor /home/meteor/installProj.sh ${PROJ}"
 #
 echo "Restarting . . . "
-ssh www.fleetingclouds.com "sudo -u root /home/meteor/restartMeteor.sh"
+#     ssh ${TARGET_SERVER} "sudo -u root /home/meteor/restartMeteor.sh"
 #
 popd > /dev/null
+
